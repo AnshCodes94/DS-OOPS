e49a9a097b94fe894f26f6bf39db6a51a040fc44
Ansh94-max <ansh122021@gmail.com>
2026-09-08 22:23:01 +0530

    Save local files before pulling

--- JAVA Questions/Practise.java
+++
@@ -1,9 +0,0 @@
-public class Practise {
-    public static void main(String args[]){
-        System.out.println("Hello world");
-        // How floating numbers stored in java
-        float f = 0.73f;
-        System.out.printf("%.20f\n",f);
-        System.out.println(f);
-    }
-}
